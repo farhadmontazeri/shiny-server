17ab8c7e246e5fde8119851875f6b73dbfbecc9a
farhadmontazeri <farhadmontazeri@gmail.com>
2017-08-01 22:09:31 -0400

    Add files via upload

--- WWW/Pnetalyzer.js
+++ WWW/Pnetalyzer.js
@@ -103,7 +103,7 @@ window.removeduplicates = function () {
 ///////////////////////////////////////////////
 
    
-   var addtooltip = function (tbl) {
+    addtooltip = function (tbl) {
        var tts= [' Little interest or pleasure in doing things ',' Feeling down, depressed, or hopeless ','Trouble falling or staying asleep, or sleeping too much ','Feeling tired or having little energy','Poor appetite or overeating',' Feeling bad about yourself '+"�"+' or that you are a failure or \
 have let yourself or your family down ',' Trouble concentrating on things, such as reading the \
 newspaper or watching television','Moving or speaking so slowly that other people could have \
